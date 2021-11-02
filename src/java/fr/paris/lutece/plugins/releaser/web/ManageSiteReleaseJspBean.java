f64ee8146612bfab7fb746adeb7b7e614bcf586b
Francois Merlin <francoiseric.merlin@paris.fr>
2021-11-02 09:50:39 +0100

    RELEASER-15 : Add RBAC to site and cluster resources

--- src/java/fr/paris/lutece/plugins/releaser/web/ManageSiteReleaseJspBean.java
+++ src/java/fr/paris/lutece/plugins/releaser/web/ManageSiteReleaseJspBean.java
@@ -207,7 +207,7 @@ public class ManageSiteReleaseJspBean extends MVCAdminJspBean
         _modifValidated = null;
         String strSiteId = request.getParameter( PARAMETER_SITE_ID );
 
-        if ( !SiteService.IsUserAuthorized( AdminUserService.getAdminUser( request ), strSiteId, SiteResourceIdService.PERMISSION_RELEASE ) )
+        if ( !SiteService.IsUserAuthorized( AdminUserService.getAdminUser( request ), _site == null?strSiteId:Integer.toString(_site.getId()), SiteResourceIdService.PERMISSION_RELEASE ) )
         {
             throw new AccessDeniedException( MESSAGE_ACCESS_DENIED );
         }
